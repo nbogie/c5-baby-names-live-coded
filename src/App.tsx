190d9aa8a9dedf3fca28b7f81ee2934cfcc93e53
nbogie <neillbogie@googlemail.com>
2022-07-26 13:52:49 +0100

    show the list of baby names, very simply

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import babyNamesData from "./babyNamesData.json";
+
+interface BabyNameData {
+  name: string,
+  id: number,
+  sex: string
+}
 
 function App() {
+
+  function makeBabyNameElement(nameData: BabyNameData) {
+    return <div>{nameData.name}</div>
+  }
+
   return (
     <div className="App">
       <h3>Hello from App</h3>
+      There are {babyNamesData.length} names.
+      <div className="mainList">
+        {babyNamesData.map(makeBabyNameElement)}
+      </div>
     </div>
   );
 }
