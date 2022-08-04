bde54277295b9f7e7695a5520e4d1693a5f8506e
nbogie <neillbogie@googlemail.com>
2022-08-04 15:25:05 +0100

    add jsdoc for props of BabyNameList

--- src/BabyNameList.tsx
+++ src/BabyNameList.tsx
@@ -2,8 +2,14 @@ import { BabyName } from "./BabyName";
 import { BabyNameData } from "./types";
 
 interface BabyNameListProps {
+  /** The baby names to show! zero or more.*/
   names: BabyNameData[];
+
+  /** Function to call if user clicks on a name.  
+   * Will be passed the relevant single BabyNameData */
   onClick: (one: BabyNameData) => void;
+
+  /** extra class to add to the element rendered */
   className: string;
 }
 
